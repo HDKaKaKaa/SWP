b05b96385a9b6811ec98810ee6d2bbc40874d6f6
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-12-04 14:12:04 +0700

    Reapply "Squashed commit of the following:"
    This reverts commit ecbf38cb21368ca912b47462c772c03db089325d.

--- backend/src/main/java/com/shopeefood/backend/entity/Owner.java
+++ backend/src/main/java/com/shopeefood/backend/entity/Owner.java
@@ -1,5 +1,9 @@
 package com.shopeefood.backend.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -17,8 +21,17 @@ public class Owner {
     @Column(name = "id_card_number")
     private String idCardNumber;
 
+    @OneToMany(mappedBy = "owner")
+    @JsonIgnore
+    private List<Restaurant> restaurants;
+
     @OneToOne
     @MapsId
     @JoinColumn(name = "account_id")
     private Account account;
+
+    public Integer getId() {
+        return accountId;
+    }
+
 }
