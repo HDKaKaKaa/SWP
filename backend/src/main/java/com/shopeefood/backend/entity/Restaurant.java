47ea5f4198c6f5b2385f168709bc479a8d9bb513
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-12-16 17:08:21 +0700

    Revert "Update entity Restaurant"
    This reverts commit 788f584e2082201a256be82f699d431252f33080.

--- backend/src/main/java/com/shopeefood/backend/entity/Restaurant.java
+++ backend/src/main/java/com/shopeefood/backend/entity/Restaurant.java
@@ -24,9 +24,6 @@ public class Restaurant {
     @Column(name = "cover_image")
     private String coverImage;
 
-    @Column(name = "license_image")
-    private String licenseImage;
-
     @OneToMany(mappedBy = "restaurant")
     @JsonManagedReference
     private List<Product> products;
@@ -40,8 +37,7 @@ public class Restaurant {
         PENDING,
         ACTIVE,
         BLOCKED,
-        REJECTED,
-        REFUNDED
+        REJECTED
     }
 
     @Enumerated(EnumType.STRING)
