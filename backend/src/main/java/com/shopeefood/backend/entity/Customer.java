a8cbe93634bd1157cce054b59041db6948307772
Phạm Nam Phương <phuongphamn814@gmail.com>
2025-12-21 08:44:46 +0700

    Update Customer.java

--- backend/src/main/java/com/shopeefood/backend/entity/Customer.java
+++ backend/src/main/java/com/shopeefood/backend/entity/Customer.java
@@ -1,8 +1,16 @@
 package com.shopeefood.backend.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
